ad95e66cf6d40a9c61afc5c57b4a03a613bc9817
malkuwari22 <59673918+malkuwari22@users.noreply.github.com>
2021-04-16 22:17:15 +0300

    Update 01-Lists.py

--- 01-Lists.py
+++ 01-Lists.py
@@ -8,3 +8,9 @@ length= len(Healthey)
 print(Healthey)
 
 print(length)
+
+print("Bnana" in Healthey) #cheking through the list "in" in the list
+if "egg" not in Healthey: #
+    print("go and Buy some egg....")
+if "bnana" not in Healthey:
+    print("go and Buy some banan....")
