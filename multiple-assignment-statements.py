5dd68f33fc635fa5e2b14741397c554d26242fdd
malkuwari22 <59673918+malkuwari22@users.noreply.github.com>
2021-04-18 04:28:20 +0300

    Add files via upload

---
+++ multiple-assignment-statements.py
@@ -0,0 +1,15 @@
+Mohammed = Abdullah = Khalid = Faisal = Salem = "Student"
+print("Mohammed Job: ",Mohammed)
+print("Abdullah Job: ", Abdullah)
+print("Khalid Job: ", Khalid)
+print("Faisal Job: ", Faisal)
+print("Salem Job: ", Salem)
+
+
+eye_color, shirt_color, age = "Black", "White", "21"
+#eye_color = "Blue"
+#shirt_color = "Black"
+#age = 23
+print("Mohammed eye color is: ", eye_color)
+print("Mohammed shirt color is: ", shirt_color)
+print("Mohammed age is: ", age)
