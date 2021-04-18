2bf11e96205bc2a61ef9cd96489e541e6e81c32d
malkuwari22 <59673918+malkuwari22@users.noreply.github.com>
2021-04-18 03:40:54 +0300

    Add files via upload

---
+++ 02-DataType.py
@@ -0,0 +1,12 @@
+#Declare a string variable
+s = 'planet Earth' 
+print (s)
+#Declare an integer variable
+x = 5 
+print(5)
+#Declare a float variable
+y = 10.17
+print(y)
+#Declare a boolean variable
+z = x > y
+print (z)
